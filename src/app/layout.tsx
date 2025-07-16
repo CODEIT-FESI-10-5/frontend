a8024821f6c20fc1eab650a385453b0c4e8739c2
AnJaeSeong <dktmzh6@gmail.com>
2025-07-16 17:57:32 +0900

    chore: 핸들러, 프로바이더 임시 저장

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,24 @@
-import type { Metadata } from "next";
-import { Noto_Sans_KR, Roboto } from "next/font/google";
-import { QueryProviders } from "./query-providers";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Noto_Sans_KR, Roboto } from 'next/font/google';
+import { QueryProviders } from './query-providers';
+import './globals.css';
+import { MSWProviders } from '@/shared/mocks/msw-provider';
 
 const notoSansKR = Noto_Sans_KR({
-  variable: "--font-noto-sans-kr",
-  subsets: ["latin"],
-  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: '--font-noto-sans-kr',
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
 });
 
 const roboto = Roboto({
-  variable: "--font-roboto",
-  subsets: ["latin"],
-  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: '--font-roboto',
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
 });
 
 export const metadata: Metadata = {
-  title: "Slid to do",
-  description: "코드잇 프론트엔드 단기 심화 과정 10기 5조 프로젝트",
+  title: 'Slid to do',
+  description: '코드잇 프론트엔드 단기 심화 과정 10기 5조 프로젝트',
 };
 
 export default function RootLayout({
@@ -28,7 +29,9 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${notoSansKR.variable} ${roboto.variable} antialiased`}>
-        <QueryProviders>{children}</QueryProviders>
+        <MSWProviders>
+          <QueryProviders>{children}</QueryProviders>
+        </MSWProviders>
       </body>
     </html>
   );
--- src/shared/mocks/handlers.ts
+++ src/shared/mocks/handlers.ts
@@ -1,7 +1,13 @@
-import { authHandlers } from "./handlers/auth";
-import { dashboardHandlers } from "./handlers/dashboard";
-import { noteHandlers } from "./handlers/note";
-import { profileHandlers } from "./handlers/profile";
-import { todoHandlers } from "./handlers/todo";
+import { authHandlers } from './handlers/auth';
+import { dashboardHandlers } from './handlers/dashboard';
+import { noteHandlers } from './handlers/note';
+import { profileHandlers } from './handlers/profile';
+import { todoHandlers } from '../../features/todo/mocks';
 
-export const handlers = [...authHandlers, ...dashboardHandlers, ...noteHandlers, ...profileHandlers, ...todoHandlers];
+export const handlers = [
+  ...authHandlers,
+  ...dashboardHandlers,
+  ...noteHandlers,
+  ...profileHandlers,
+  ...todoHandlers,
+];
