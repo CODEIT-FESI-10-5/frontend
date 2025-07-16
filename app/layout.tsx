3fb95d840ed7bbac392b58ae51c8526ecd42fb2a
memymini <minhi0614@gmail.com>
2025-07-16 16:30:21 +0900

    chore: msw provider 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,10 +29,9 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${notoSansKR.variable} ${roboto.variable} antialiased`}>
-        <QueryProvider>
-          <MswProvider />
-          {children}
-        </QueryProvider>
+        <MswProvider>
+          <QueryProvider>{children}</QueryProvider>
+        </MswProvider>
       </body>
     </html>
   );
