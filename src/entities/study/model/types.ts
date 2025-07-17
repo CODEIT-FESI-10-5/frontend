fcf6e947c5e694d096e688ed5d3f40bbe8b35a46
memymini <minhi0614@gmail.com>
2025-07-17 09:19:34 +0900

    feat:study, user mock data 추가

--- src/entities/study/model/types.ts
+++ src/entities/study/model/types.ts
@@ -1,24 +1,3 @@
-// 로그인 요청
-export interface LoginRequest {
-  email: string;
-  password: string;
-}
-
-// 회원가입 요청
-export interface SignupRequest {
-  name: string;
-  email: string;
-  password: string;
-}
-
-// 프로필 응답
-export interface ProfileResponse {
-  id: string;
-  name: string;
-  email: string;
-  image: string;
-}
-
 // 스터디 목록 응답
 export interface StudyListResponse {
   studyList: StudyItem[];
@@ -29,7 +8,7 @@ export interface StudyItem {
   title: string;
   description: string;
   isLeader: boolean;
-  studyGoal: string[];
+  studyGoals: string[];
 }
 
 // 스터디 생성 응답
