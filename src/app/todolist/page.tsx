7d78cdb5faddd2b23cbc309c3c5b9bdb232d574d
AnJaeSeong <dktmzh6@gmail.com>
2025-07-17 20:05:48 +0900

    remove: 파일, 폴더 이동 및 제거사항 적용

--- src/app/todolist/page.tsx
+++
@@ -1,9 +0,0 @@
-import TodolistPanel from '@/features/todo/TodolistPanel';
-
-export default function Todolist() {
-  return (
-    <div className="bg-surface-100 flex min-h-screen gap-4 p-8 text-black">
-      <TodolistPanel />
-    </div>
-  );
-}
