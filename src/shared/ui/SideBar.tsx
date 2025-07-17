65208a1baf3d12fe82c2644c91a2b61c63d0084b
memymini <minhi0614@gmail.com>
2025-07-17 11:51:18 +0900

    chore:sidebar button 추가

--- src/shared/ui/SideBar.tsx
+++ src/shared/ui/SideBar.tsx
@@ -1,10 +1,16 @@
 import Profile from '@/features/sidebar/profile/ui/Profile';
+import CreateStudyButton from '@/features/sidebar/button/ui/CreateStudyButton';
+import JoinButton from '@/features/sidebar/button/ui/JoinStudyButton';
 
 export default function SideBar() {
   return (
     <div className="bg-surface-2 fixed h-screen w-348 p-26">
       <div>logo</div>
       <Profile />
+      <div className="mt-24 flex justify-between">
+        <CreateStudyButton />
+        <JoinButton />
+      </div>
     </div>
   );
 }
