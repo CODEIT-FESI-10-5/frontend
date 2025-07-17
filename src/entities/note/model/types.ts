a1daa68648c9f7997b7495fbc1ca3769cee7f839
JustDevRae <ksrae165@gmail.com>
2025-07-17 12:37:37 +0900

    feat: 노트 목록 목데이터 생성

--- src/entities/note/model/types.ts
+++ src/entities/note/model/types.ts
@@ -1,55 +1,11 @@
 // 노트 목록 응답
-export interface NotesListResponse {
-  totalCount: number;
-  notes: NoteItem[];
-}
-
-export interface NoteItem {
+export interface Note {
   id: number;
   title: string;
   content: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-// 노트 상세 응답
-export interface NoteDetailResponse {
-  id: number;
-  title: string;
-  content: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-// 노트 생성 요청
-export interface CreateNoteRequest {
-  title: string;
-  content: string;
-}
-
-// 노트 생성 응답
-export interface CreateNoteResponse {
-  id: number;
-  title: string;
-  content: string;
-}
-
-// 노트 수정 요청
-export interface UpdateNoteRequest {
-  title?: string;
-  content?: string;
-}
-
-// 노트 수정 응답
-export interface UpdateNoteResponse {
-  id: number;
-  title: string;
-  content: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-// 노트 검색 쿼리 파라미터
-export interface NoteSearchParams {
-  note_search_query: string;
+  studyGoalId: number;
+  todoId: number;
+  todoTitle: string;
+  createdAt: string;
+  updatedAt: string;
 }
