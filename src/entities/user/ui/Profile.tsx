97c7f366aa37db92f16039decddfec483301d6bc
memymini <minhi0614@gmail.com>
2025-07-18 10:33:54 +0900

    styles:프로필 스타일 수정

--- src/entities/user/ui/Profile.tsx
+++ src/entities/user/ui/Profile.tsx
@@ -6,9 +6,11 @@ export default function Profile() {
   if (isLoading) return <div>로딩중</div>;
   if (error) return <div>오류발생</div>;
   return (
-    <div className="bg-surface-4 h-79 w-full">
-      <div>{data?.image ? <img src={data.image} /> : <div />}</div>
-      <div>
+    <div className="bg-surface-4 flex h-79 w-full items-center gap-13 px-14 py-12">
+      <div className="rounded-100 bg-text-tertiary h-55 w-55">
+        {data?.image ? <img src={data.image} /> : <div />}
+      </div>
+      <div className="h-43">
         <p className="body-large text-text-secondary">{data?.name}</p>
         <p className="label-small text-text-secondary">{data?.email}</p>
       </div>
