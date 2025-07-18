c559b893d0b8119630e5393fd957faf267babb2b
memymini <minhi0614@gmail.com>
2025-07-18 15:12:49 +0900

    chore:auth 레이아웃 수정

--- src/features/auth/ui/AuthLayout.tsx
+++ src/features/auth/ui/AuthLayout.tsx
@@ -10,14 +10,17 @@ export default function AuthLayout({
           <div className="mt-154 h-287 w-548 bg-gray-300"></div>
         </div>
         <div className="flex flex-1 items-center justify-center">
-          <div className="flex h-full w-442 flex-col">
-            <h1 className="headline-large text-text-white">
-              슬리드 투두에 오신걸 환경합니다!
-            </h1>
-            <p className="text-text-secondary label-large">
-              혼자보다 함께. 슬리드투두로 팀의 스터디 상황을 공유하세요.
-            </p>
-            <div className="w-full max-w-md">{children}</div>
+          <div className="flex h-full w-442 flex-col gap-60">
+            <div>
+              <h1 className="headline-large text-text-white">
+                슬리드 투두에 오신걸 환경합니다!
+              </h1>
+              <p className="text-text-secondary label-large">
+                혼자보다 함께. 슬리드투두로 팀의 스터디 상황을 공유하세요.
+              </p>
+            </div>
+
+            <div className="">{children}</div>
           </div>
         </div>
       </div>
