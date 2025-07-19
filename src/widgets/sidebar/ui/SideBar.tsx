87fdb52d91d625d7a5fd0a1e324bd72e4711f750
memymini <minhi0614@gmail.com>
2025-07-19 22:35:52 +0900

    feat:노트페이지 이동시 ui 변경 기능 추가

--- src/widgets/sidebar/ui/SideBar.tsx
+++ src/widgets/sidebar/ui/SideBar.tsx
@@ -1,19 +1,13 @@
-import { Profile } from '@/features/get-profile/ui';
-import { CreateStudyButton } from '@/features/create-study/ui';
-import JoinStudyButton from '@/widgets/sidebar/ui/JoinStudyButton';
 import { StudyGoalList } from '@/features/get-goal-list/ui';
 import { StudyList } from '@/features/get-study-list/ui';
+import SideBarInfo from '@/widgets/sidebar/ui/SideBarInfo';
 
 export default function SideBar() {
   return (
     <div className="bg-surface-2 fixed flex h-screen w-348 flex-col gap-64 p-26">
       <div>
         <img src="/images/logo.png" alt="logo" className="mb-28 h-28 w-115" />
-        <Profile />
-        <div className="mt-24 flex justify-between">
-          <CreateStudyButton />
-          <JoinStudyButton />
-        </div>
+        <SideBarInfo />
       </div>
       <StudyList />
       <StudyGoalList />
