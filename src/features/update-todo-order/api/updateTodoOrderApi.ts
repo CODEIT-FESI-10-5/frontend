7466181992172983dda2174df51262d3b9bd5e76
AnJaeSeong <dktmzh6@gmail.com>
2025-07-22 16:22:36 +0900

    fix: update order querykey 적용, invalidate 제거

--- src/features/update-todo-order/api/updateTodoOrderApi.ts
+++ src/features/update-todo-order/api/updateTodoOrderApi.ts
@@ -4,7 +4,7 @@ export const updateTodoOrder = async (
   goalId: string,
   newOrder: Array<string>,
 ) => {
-  const endpoint = `/api/goal/${goalId}/order`;
+  const endpoint = `/api/goal/${goalId}/todolist/order`;
   const parsedResponse = await clientFetch.patch(endpoint, {
     newOrder: newOrder,
   });
