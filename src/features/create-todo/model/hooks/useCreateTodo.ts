b5f5a45ecab72690595bf02fc4f61f078c2f8327
AnJaeSeong <dktmzh6@gmail.com>
2025-07-23 14:16:59 +0900

    feat: TodoCustomMutation에서 mutationOption을 주입받을 수 있도록 변경, createTodo에서 성공 실패시 토스트 알림 추가

--- src/features/create-todo/model/hooks/useCreateTodo.ts
+++ src/features/create-todo/model/hooks/useCreateTodo.ts
@@ -2,6 +2,7 @@
 import { useTodoCustomMutation } from '@/shared/lib/utils/useTodoCustomMutation';
 import { createTodo, newTodoData } from '../../api';
 import { todolistQueryKeys } from '@/entities/todolist/model';
+import toast from 'react-hot-toast';
 
 interface CreateTodoMutationParams {
   newTodo: newTodoData;
@@ -11,4 +12,12 @@ export const useCreateTodoMutation = (goalId: string) =>
   useTodoCustomMutation<CreateTodoMutationParams, any>(
     ({ newTodo }) => createTodo(goalId, newTodo),
     [...todolistQueryKeys.todolist(goalId)],
+    {
+      onSuccess: () => {
+        toast.success('투두 생성에 성공했습니다');
+      },
+      onError: () => {
+        toast.error('투두 생성에 실패했습니다');
+      },
+    },
   );
