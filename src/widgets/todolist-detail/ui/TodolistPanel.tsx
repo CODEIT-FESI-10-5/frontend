944b2cdeb269e2bc1b873a3320b0da1fc0c22715
AnJaeSeong <dktmzh6@gmail.com>
2025-07-23 17:17:14 +0900

    feat: 투두 개수 10개 이상 생성 불가 기능 추가 및 todolist-deatil page ui수정

--- src/widgets/todolist-detail/ui/TodolistPanel.tsx
+++ src/widgets/todolist-detail/ui/TodolistPanel.tsx
@@ -3,9 +3,7 @@
 import { cn } from '@/shared/lib/utils/cn';
 import { LayoutGroup, motion } from 'framer-motion';
 import NewTodo from '@/../public/assets/new-todo.svg';
-import IconBack from '@/../public/assets/icon-back.svg';
 import { useCreateTodoStore } from '../../../features/create-todo/model/store';
-import Link from 'next/link';
 import Todolist from './Todolist';
 import { useTodolistQuery } from '@/entities/todolist/model/hooks';
 import divideTodoGroup from '@/entities/todolist/lib/utils/divideTodoGroup';
@@ -13,23 +11,11 @@ import { useEffect } from 'react';
 import { useTodolistStore } from '@/entities/todolist/model/store';
 import { useParams } from 'next/navigation';
 import ConfirmButton from '@/features/create-todo/ui/ConfirmButton';
-
-function BackButton({ studyId, goalId }: { studyId: string; goalId: string }) {
-  return (
-    <motion.div layout={'position'}>
-      <Link
-        href={`/dashbord/${studyId}/goal/${goalId}`}
-        className="mx-10 flex cursor-pointer items-center gap-30 text-white transition hover:scale-105"
-      >
-        <IconBack stroke="white" />
-        <p className="title-large">전체 투두 리스트</p>
-      </Link>
-    </motion.div>
-  );
-}
+import toast from 'react-hot-toast';
 
 function TitleArea({ title = '목표' }: { title?: string }) {
   const toggleEditMode = useCreateTodoStore((state) => state.toggleEditMode);
+  const getTodoCount = useTodolistStore((state) => state.getTodoCount);
   return (
     <motion.div
       layout={'position'}
@@ -37,9 +23,16 @@ function TitleArea({ title = '목표' }: { title?: string }) {
     >
       <p className="headline-large font-bold text-white">{title}</p>
       <ConfirmButton
+        aria-label="open-todo-form-btn"
         size="lg"
         color="bg-highlight"
-        onClick={() => toggleEditMode()}
+        onClick={() => {
+          if (getTodoCount() >= 10) {
+            toast.error('투두는 최대 10개까지 생성 가능합니다');
+            return;
+          }
+          toggleEditMode();
+        }}
       >
         <NewTodo width={24} height={24} />
         <p className="label-small">세부 투두 생성</p>
@@ -66,19 +59,13 @@ export default function TodolistPanel() {
     <LayoutGroup>
       <motion.div
         layout
-        className="bg-surface-1 flex h-full w-full flex-col items-start gap-50 rounded-md px-30 py-40"
+        className={cn(
+          'relative flex w-740 flex-col',
+          'bg-surface-2 rounded-sm px-34 py-40 text-black shadow-lg',
+        )}
       >
-        <BackButton studyId={params.studyId} goalId={params.goalId} />
-        <motion.div
-          layout
-          className={cn(
-            'relative flex w-740 flex-col',
-            'bg-surface-3 rounded-lg p-40 text-black shadow-lg',
-          )}
-        >
-          <TitleArea title={data?.title} />
-          <Todolist />
-        </motion.div>
+        <TitleArea title={data?.title} />
+        <Todolist />
       </motion.div>
     </LayoutGroup>
   );
