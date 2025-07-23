c9a2a1b15e4eb5e841ec959919e300d3f4e41290
memymini <minhi0614@gmail.com>
2025-07-23 13:12:28 +0900

    chore:auth layout 변경

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -6,38 +6,9 @@ export default function AuthLayout({
 }) {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <div className="bg-surface-2 flex h-880 w-1306 items-center justify-center">
-        <AuthIntro />
-        <div className="flex flex-1 items-center justify-center">
-          <div className="flex h-full w-442 flex-col gap-60">
-            <div className="flex flex-col gap-28">
-              <Image
-                src="/images/logo.png"
-                width={153}
-                height={36}
-                alt="logo"
-              />
-              <div>
-                <h1 className="headline-large text-text-white">
-                  슬리드 투두에 오신걸 환영합니다!
-                </h1>
-                <p className="text-text-secondary label-large">
-                  혼자보다 함께. 슬리드투두로 팀의 스터디 상황을 공유하세요.
-                </p>
-              </div>
-            </div>
-            <div className="">{children}</div>
-          </div>
-        </div>
+      <div className="bg-surface-2 flex h-auto w-642 items-center justify-center px-100 py-68">
+        <div className="">{children}</div>
       </div>
-    </div>
-  );
-}
-
-function AuthIntro() {
-  return (
-    <div className="bg-tertiary hidden h-848 w-629 flex-col items-center md:flex">
-      <div className="mt-154 h-287 w-548 bg-gray-300"></div>
     </div>
   );
 }
