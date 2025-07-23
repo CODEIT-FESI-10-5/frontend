6bf2540bdb651e11f421fdbb5e2cf4d77107af30
memymini <minhi0614@gmail.com>
2025-07-23 16:29:00 +0900

    fix:이미지 Image 컴포넌트 사용하도록 수정

--- src/widgets/sidebar/ui/SideBar.tsx
+++ src/widgets/sidebar/ui/SideBar.tsx
@@ -2,6 +2,7 @@
 import { StudyGoalList } from '@/features/get-goal-list/ui';
 import { StudyDropDown, StudyList } from '@/features/get-study-list/ui';
 import SideBarInfo from '@/widgets/sidebar/ui/SideBarInfo';
+import Image from 'next/image';
 import { useState } from 'react';
 
 export default function SideBar() {
@@ -12,7 +13,13 @@ export default function SideBar() {
   return (
     <div className="bg-surface-2 fixed top-0 bottom-0 left-0 flex w-348 flex-col gap-64 p-26">
       <div>
-        <img src="/images/logo.png" alt="logo" className="mb-28 h-28 w-115" />
+        <Image
+          src="/images/logo.png"
+          alt="logo"
+          width={115}
+          height={28}
+          className="mb-28"
+        />
         <SideBarInfo />
       </div>
       <div className="flex flex-col gap-16">
