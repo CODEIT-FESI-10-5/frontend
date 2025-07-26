ec3cdbcc7d19b0e86c16a0e8864e84167d74361d
memymini <minhi0614@gmail.com>
2025-07-26 20:34:23 +0900

    chore:로그인 response응답 제거

--- src/entities/auth/api/requestLogin.ts
+++ src/entities/auth/api/requestLogin.ts
@@ -2,5 +2,5 @@ import { clientFetch } from '@/shared/api';
 import { LoginRequestApi, LoginResponseApi } from '../model';
 
 export const requestLogin = (data: LoginRequestApi) => {
-  return clientFetch.post<LoginResponseApi>('/api/auth/login', data);
+  return clientFetch.post('/api/login', data);
 };
