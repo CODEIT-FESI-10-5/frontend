faf30defca291b25531ac0408beb0084fbd9c195
sdc9787 <sfc9787@gmail.com>
2025-07-28 14:14:39 +0900

    fix : sidebar, dashboard api 수정및 구조 변경

--- src/entities/goal/model/queryKeys.ts
+++ src/entities/goal/model/queryKeys.ts
@@ -1,5 +1,4 @@
 export const goalQueryKeys = {
   all: ['goal'] as const,
-  list: () => [...goalQueryKeys.all, 'list'] as const,
-  detail: (goalId: string) => [...goalQueryKeys.all, 'detail', goalId] as const,
+  list: (studyId: number) => [...goalQueryKeys.all, 'list', studyId] as const,
 } as const;
--- src/features/get-study-list/api/handler.ts
+++ src/features/get-study-list/api/handler.ts
@@ -2,7 +2,7 @@ import { http, HttpResponse } from 'msw';
 import { mockStudyListResponseApi } from '@/entities/study/model/__mocks__/study.mock';
 
 export const sidebarStudyHandler = [
-  http.get('/api/sidebar/study-list', () => {
+  http.get('/api/study', () => {
     return HttpResponse.json(mockStudyListResponseApi);
   }),
 ];
