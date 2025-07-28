850e7f29ec519475bdc1d346d719bbfeefcafdcc
minhee <133033494+memymini@users.noreply.github.com>
2025-07-28 17:09:45 +0900

    Revert "chore:사이드바 스터디 생성 및 스터디 리스트 생성 부분 수정"

--- src/features/create-study/model/useCreateStudy.ts
+++ src/features/create-study/model/useCreateStudy.ts
@@ -1,20 +1,11 @@
 import { useMutation } from '@tanstack/react-query';
 import { postCreateStudy } from '@/entities/study/api/postCreateStudy';
-import { CreateStudyResponseApi } from '@/entities/study/model/types';
 
 export const useCreateStudy = (
-  onSuccess?: (newStudyId: number) => void,
-  onError?: (error: unknown) => void,
+  onSuccess?: (data: { newStudyId: string }) => void,
 ) => {
-  return useMutation<CreateStudyResponseApi, unknown, void>({
+  return useMutation({
     mutationFn: postCreateStudy,
-    onSuccess: (res) => {
-      if (res.httpStatusCode === 200 && res.errorCode === 'SUCCESS') {
-        onSuccess?.(res.data.studyId);
-      }
-    },
-    onError: (err) => {
-      onError?.(err);
-    },
+    onSuccess,
   });
 };
