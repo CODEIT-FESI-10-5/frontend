eab2ef871f00eb7076b3824b60d3a94a6faff863
memymini <minhi0614@gmail.com>
2025-07-29 19:08:27 +0900

    chore: useStudyQuery에 enabled 옵션 추가

--- src/entities/goal/model/useGetGoal.ts
+++ src/entities/goal/model/useGetGoal.ts
@@ -2,10 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 import { getGoalList } from '@/entities/goal/api/getGoalList';
 import { GoalListResponseApi, goalQueryKeys } from '@/entities/goal';
 
-export const useGetGoal = (studyId: number) => {
+export const useGetGoal = (
+  studyId: number,
+  options?: { enabled?: boolean },
+) => {
   return useQuery<GoalListResponseApi, Error, GoalListResponseApi['data']>({
     queryKey: goalQueryKeys.list(studyId),
     queryFn: () => getGoalList(studyId),
     select: (response) => response.data,
+    enabled: options?.enabled ?? true,
   });
 };
