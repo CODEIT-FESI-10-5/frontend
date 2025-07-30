6ae2bfb39b67007ae48d9e943b033cddf0b0942e
memymini <minhi0614@gmail.com>
2025-07-30 13:02:23 +0900

    chore:setStudyId 문제 해결

--- src/features/redirect/model/useDashboardRedirect.ts
+++ src/features/redirect/model/useDashboardRedirect.ts
@@ -13,7 +13,6 @@ export function useDashboardRedirect() {
 
   // study가 있는지 검사
   const studyId = currentStudyId || studyData?.studyList?.[0]?.id || '';
-  setStudyId(String(studyId));
 
   // goalId가 있는지 검사 (studyId가 존재하는 경우)
   const { data: goalData } = useGetGoal(Number(studyId), {
@@ -22,24 +21,29 @@ export function useDashboardRedirect() {
   const goalId = studyId ? getLastVisitedGoalId(studyId) : null;
 
   useEffect(() => {
+    setStudyId(String(studyId));
     // 0. 스터디 데이터가 아예 안받아져오는 경우 처리 필요
     if (!studyData) router.push('/');
     // 1. 스터디가 없는 경우 홈으로 이동
     if (studyData?.totalCount === 0) {
+      console.log('redirect state:', 1);
       router.replace('/dashboard/study');
     }
     // 2. goal 리스트가 없으면 goal 생성 화면으로
     if (goalData && goalData.totalCount === 0) {
       router.replace(`/dashboard/study/${studyId}`);
+      console.log('redirect state:', 2);
       return;
     }
     // 3. store에 goal이 없으면 첫번째 goal로 이동
     if (!goalId) {
       const initialGoalId = String(goalData?.goals[0].id);
       router.replace(`/dashboard/study/${studyId}/goal/${initialGoalId}`);
+      console.log('redirect state:', 3);
       return;
     }
     // 4. 모든 조건이 있는 경우
+    console.log('redirect state:', 4);
     router.replace(`/dashboard/study/${studyId}/goal/${goalId})}`);
   }, []);
 }
