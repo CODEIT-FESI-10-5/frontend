5dbda3a627552280c86466cfab99bb327ffa3c85
memymini <minhi0614@gmail.com>
2025-07-31 15:26:24 +0900

    chore:useJoinStudy 훅 쿼리 업데이트 코드 추가

--- src/features/join-study/model/useJoinStudy.ts
+++ src/features/join-study/model/useJoinStudy.ts
@@ -1,14 +1,23 @@
 'use client';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { postJoinStudy } from '@/entities/study/api';
 import { useRouter } from 'next/navigation';
 import toast from 'react-hot-toast';
+import { studyQueryKeys } from '@/entities/study';
+import { goalQueryKeys } from '@/entities/goal';
 
 export const useJoinStudy = () => {
+  const queryClient = useQueryClient();
   const router = useRouter();
   return useMutation({
     mutationFn: postJoinStudy,
     onSuccess: (data: { studyId: string }) => {
+      queryClient.invalidateQueries({
+        queryKey: studyQueryKeys.list(),
+      });
+      queryClient.invalidateQueries({
+        queryKey: goalQueryKeys.list(Number(data.studyId)),
+      });
       router.push(`/dashboard/study/${data.studyId}`);
     },
     onError: (err: unknown) => {
