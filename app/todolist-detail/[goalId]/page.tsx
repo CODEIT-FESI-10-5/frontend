72bce6a72dc3b6f0d2726734c2b24a4437479b6e
AnJaeSeong <dktmzh6@gmail.com>
2025-08-01 10:56:05 +0900

    feat: todolist-detail 페이지 tanstack query prefetch 적용

--- app/todolist-detail/[goalId]/page.tsx
+++ app/todolist-detail/[goalId]/page.tsx
@@ -1,5 +1,25 @@
+import { getQueryClient } from '@/app/getQueryClient';
+import { fetchTodolist } from '@/entities/todolist/api';
+import { todolistQueryKeys } from '@/entities/todolist/model';
 import TodolistPage from '@/pages/todolist-detail/TodolistPage';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
-export default function Page() {
-  return <TodolistPage />;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ goalId: string }>;
+}) {
+  const goalId = (await params).goalId;
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: todolistQueryKeys.todolist(goalId),
+    queryFn: () => fetchTodolist(goalId),
+  });
+  // console.log(queryClient.getQueryData(todolistQueryKeys.todolist(goalId)));
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <TodolistPage />
+    </HydrationBoundary>
+  );
 }
