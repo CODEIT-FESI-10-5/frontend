bccd66d220062c1888797a7ea7b745a5a9171b8a
memymini <minhi0614@gmail.com>
2025-08-01 16:24:48 +0900

    chore:appbar 임시 구현

---
+++ src/shared/ui/AppBar.tsx
@@ -0,0 +1,44 @@
+'use client';
+import { usePathname, useRouter } from 'next/navigation';
+import { useDrawerStore } from '@/widgets/sidebar/ui/model';
+import BackIcon from '@/assets/icon-back2.svg';
+import CloseIcon from '@/assets/icon-close.svg';
+import MenuIcon from '@/assets/icon-menu.svg';
+interface AppBarProps {
+  pageName: string;
+}
+
+export default function AppBar({ pageName }: AppBarProps) {
+  const { open } = useDrawerStore();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.back();
+  };
+
+  const isNote = pathname === '/note';
+
+  return (
+    <>
+      {isNote ? (
+        <div className="relative flex h-54 w-full items-center justify-center xl:hidden">
+          <MenuIcon className="absolute left-18 h-24 w-24" onClick={open} />
+          <h1 className="text-text-white">{pageName}</h1>
+          <CloseIcon
+            className="absolute right-18 h-26 w-26"
+            onClick={handleBack}
+          />
+        </div>
+      ) : (
+        <div className="relative flex h-54 w-full items-center justify-center xl:hidden">
+          <BackIcon
+            className="absolute left-18 h-24 w-24"
+            onClick={handleBack}
+          />
+          <h1 className="text-text-white">{pageName}</h1>
+        </div>
+      )}
+    </>
+  );
+}
