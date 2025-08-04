d6f20695743c84d0e67f4c5d312fffa73773e6b0
sdc9787 <sfc9787@gmail.com>
2025-08-04 11:25:32 +0900

    style : figma 에 맞춰서 스타일 변경

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -17,37 +17,36 @@ export default function NotFound() {
   };
 
   return (
-    <div className="bg-surface-4 flex min-h-screen w-full flex-col items-center justify-center px-20">
+    <div className="bg-surface-2 flex min-h-screen w-full flex-col items-center justify-center px-20">
       {/* 404 이미지 */}
-      <div className="mb-40">
+      <div className="">
         <Image
           src="/images/404.png"
           alt="404 Error"
-          width={400}
-          height={300}
-          style={{ height: 'auto', width: 'auto' }}
-          className="max-w-[300px] sm:max-w-[400px]"
+          width={502}
+          height={227}
+          className="max-w-[328px] md:max-w-[502px]"
         />
       </div>
 
       {/* 404 메인 콘텐츠 */}
-      <div className="flex flex-col items-center text-center">
+      <div className="flex w-full flex-col items-center text-center">
         {/* 메시지 */}
         <div className="mb-40">
           <h2
             className={cn(
-              'mb-12 font-bold text-white',
-              'text-[20px]',
-              'sm:text-[24px]',
+              'mb-12 text-white',
+              'm-headline-medium',
+              'md:headline-medium',
             )}
           >
             페이지를 찾을 수 없습니다
           </h2>
           <p
             className={cn(
-              'font-normal text-[#adb5bd]',
-              'text-[14px] leading-relaxed',
-              'sm:text-[16px]',
+              'text-text-secondary font-normal',
+              'm-label-small',
+              'md:body-small',
             )}
           >
             요청하신 페이지가 존재하지 않거나
@@ -56,20 +55,18 @@ export default function NotFound() {
           </p>
         </div>
 
+        {/*todo 버튼 공용 컴포넌트으로 변경 */}
         {/* 버튼 영역 */}
-        <div className="flex w-full flex-col gap-12 sm:flex-row sm:justify-center sm:gap-16">
-          <button
-            onClick={handleGoBack}
-            className={cn(
-              'bg-primary transition-colors duration-200 hover:bg-[#6c79e8]',
-              'rounded-lg px-32 py-16 font-medium text-white',
-              'text-[14px] sm:text-[16px]',
-              'w-full sm:w-auto sm:min-w-[378px]',
-            )}
-          >
-            뒤로가기
-          </button>
-        </div>
+        <button
+          onClick={handleGoBack}
+          className={cn(
+            'bg-primary w-full cursor-pointer rounded-lg py-16 text-white transition-colors duration-200 hover:bg-[#6c79e8]',
+            'm-body-medium',
+            'md:body-medium md:max-w-[378px]',
+          )}
+        >
+          뒤로가기
+        </button>
       </div>
 
       {/* 하단 설명 */}
