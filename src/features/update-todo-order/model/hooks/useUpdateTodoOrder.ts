3296cd224d09cbf695449bd120d16ff5751c7e09
AnJaeSeong <dktmzh6@gmail.com>
2025-08-05 15:51:03 +0900

    fix: update todo order에서 useCustomMutation 참조 오류 수정

--- src/features/update-todo-order/model/hooks/useUpdateTodoOrder.ts
+++ src/features/update-todo-order/model/hooks/useUpdateTodoOrder.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useTodoCustomMutation } from '@/shared/lib/utils/useCustomMutation';
+import { useCustomMutation } from '@/shared/lib/utils/useCustomMutation';
 import { updateTodoOrder } from '../../api';
 import { dashboardQueryKeys } from '@/entities/dashboard';
 import { todolistQueryKeys } from '@/entities/todolist/model';
@@ -10,7 +10,7 @@ interface UpdateTodoOrderMutationParams {
 }
 
 export const useUpdateTodoOrderMutation = (goalId: string) =>
-  useTodoCustomMutation<UpdateTodoOrderMutationParams, any>({
+  useCustomMutation<UpdateTodoOrderMutationParams, any>({
     mutationFn: ({ todoId, newOrder }) => updateTodoOrder({ todoId, newOrder }),
     invalidateQueryKeys: [
       [...dashboardQueryKeys.goal(goalId)],
