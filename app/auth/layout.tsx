bbb41d3c7c106e1b1bf87f3722175c67b75cc728
memymini <minhi0614@gmail.com>
2025-08-05 11:48:28 +0900

    chore:auth 페이지 배경색 수정

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-full w-full max-w-screen items-start justify-center overflow-y-scroll px-24 py-32 md:items-center md:p-0">
+    <div className="bg-surface-2 md:bg-surface-1 flex h-full w-full max-w-screen items-start justify-center overflow-y-scroll px-24 py-32 md:items-center md:p-0">
       <div className="md:bg-surface-2 rounded-12 flex w-327 flex-col items-start justify-center gap-28 overflow-y-auto md:h-auto md:w-642 md:px-100 md:py-68">
         <div className="relative h-24 w-94 md:h-36 md:w-153">
           <Image
