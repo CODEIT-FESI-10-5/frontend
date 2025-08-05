6c63e2cea2e45f1aca50c336c53d7d7ca00c73a9
sdc9787 <sfc9787@gmail.com>
2025-08-05 16:46:09 +0900

    feat : [goal title, study image, study info] customMutation으로 교체

--- src/features/update-goal-title/model/useUpdateGoalTitleMutation.ts
+++ src/features/update-goal-title/model/useUpdateGoalTitleMutation.ts
@@ -1,25 +1,23 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updateGoalTitle } from '../api/updateGoalTitle';
-import toast from 'react-hot-toast';
+import { useCustomMutation } from '@/shared/lib/utils/useCustomMutation';
 
 export function useUpdateGoalTitleMutation(
   studyId: number,
   goalId: string,
   setTitle: (title: string) => void,
 ) {
-  const queryClient = useQueryClient();
-  return useMutation({
+  return useCustomMutation({
     mutationFn: (newTitle: string) => updateGoalTitle(goalId, newTitle),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['dashboard', goalId] });
-      queryClient.invalidateQueries({ queryKey: ['goal', 'list', studyId] });
-      queryClient.invalidateQueries({ queryKey: ['todolist', goalId] });
-
-      toast.success('제목이 업데이트되었습니다');
-    },
-    onError: (error, newTitle) => {
-      toast.error('제목 업데이트에 실패했습니다');
-      setTitle(newTitle); // 실패 시 원래 제목으로 복구
+    invalidateQueryKeys: [
+      ['dashboard', goalId],
+      ['goal', 'list', studyId],
+      ['todolist', goalId],
+    ],
+    successMessage: '제목이 업데이트되었습니다',
+    mutationOptions: {
+      onError: (error, newTitle) => {
+        setTitle(newTitle); // 실패 시 원래 제목으로 복구
+      },
     },
   });
 }
