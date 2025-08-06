25bb113ddaca8609f5720e9a5a93ff85d0724477
AnJaeSeong <dktmzh6@gmail.com>
2025-08-06 11:10:20 +0900

    design: Button 공통 컴포넌트 스타일 수정

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -4,26 +4,30 @@ import { cn } from '../utils/cn';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: React.ReactNode;
   size: 'lg' | 'md' | 'sm' | 'xs';
-  theme: 'primary' | 'tertiary' | 'highlight' | 'surface';
+  theme: 'primary' | 'tertiary' | 'highlight' | 'surface' | 'emphasis';
   className?: string;
 }
 
-const buttonVariants = cva('flex items-center justify-center text-text-white', {
-  variants: {
-    size: {
-      lg: 'py-16 h-55 w-full body-medium rounded-6',
-      md: 'px-18 py-13 w-fit h-50 title-small rounded-6',
-      sm: 'px-16 py-8 w-fit h-40 body-medium rounded-4',
-      xs: 'px-12 py-6 w-fit h-32 label-large rounded-4',
-    },
-    theme: {
-      primary: 'bg-primary',
-      tertiary: 'bg-tertiary',
-      highlight: 'bg-highlight',
-      surface: 'bg-surface-2',
+const buttonVariants = cva(
+  'flex items-center justify-center text-text-white cursor-pointer',
+  {
+    variants: {
+      size: {
+        lg: 'py-16 h-55 w-full m-title-small md:body-medium rounded-6',
+        md: 'px-18 py-13 w-fit h-50 m-title-small md:title-small rounded-6',
+        sm: 'px-16 py-8 w-fit h-40 m-body-medium md:body-medium rounded-4',
+        xs: 'px-12 py-6 w-fit h-32 m-body-small md:label-small  rounded-4',
+      },
+      theme: {
+        primary: 'bg-primary',
+        tertiary: 'bg-tertiary',
+        highlight: 'bg-highlight',
+        surface: 'bg-surface-2',
+        emphasis: 'bg-border-emphasis',
+      },
     },
   },
-});
+);
 
 const disabledVariants = cva('', {
   variants: {
