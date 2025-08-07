f305d94a686a06c61d88c9f6a73d1a453901bd6f
memymini <minhi0614@gmail.com>
2025-08-07 18:15:23 +0900

    chore:회원정보 변경시 storage 업데이트

--- src/features/update-account/model/useUpdateProfile.ts
+++ src/features/update-account/model/useUpdateProfile.ts
@@ -1,17 +1,21 @@
 'use client';
 
 import { updateProfile } from '@/entities/profile/api';
-import { UpdateProfileRequestApi } from '@/entities/profile/model';
+import {
+  UpdateProfileRequestApi,
+  useProfileStore,
+} from '@/entities/profile/model';
 import { useCustomMutation } from '@/shared/lib/utils/useCustomMutation';
 
 export function useUpdateProfile() {
+  const setProfileImg = useProfileStore((state) => state.setNickname);
   return useCustomMutation({
     mutationFn: (data: UpdateProfileRequestApi) => updateProfile(data),
     successMessage: '이미지가 변경되었습니다.',
     mutationOptions: {
       onSuccess: (res) => {
         const newProfileUrl = res.data.profileImg;
-        localStorage.setItem('profileImg', newProfileUrl);
+        setProfileImg(newProfileUrl);
       },
     },
   });
