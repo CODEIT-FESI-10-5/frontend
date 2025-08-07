fc02ea978e61c0f69f0e22b805034680db233e25
sdc9787 <sfc9787@gmail.com>
2025-08-07 14:09:07 +0900

    feat : delete-study index 추가

--- src/features/delete-study/model/useDeleteStudy.ts
+++ src/features/delete-study/model/useDeleteStudy.ts
@@ -13,8 +13,11 @@ export const useDeleteStudyMutation = (studyId: string) =>
   useCustomMutation<DeleteStudyMutationParams, any>({
     mutationFn: ({ studyId }) => deleteStudy(studyId),
     invalidateQueryKeys: [
+      //대시보드 쿼리 무효화
       [...dashboardQueryKeys.goal(studyId)],
+      //스터디 리스트 쿼리 무효화
       [...studyQueryKeys.list()],
+      //목표 리스트 쿼리 무효화
       [...goalQueryKeys.list(Number(studyId))],
     ],
     successMessage: '스터디가 삭제되었습니다',
