20ff0113510ade37a0604461820ff0a368dac588
sdc9787 <sfc9787@gmail.com>
2025-08-07 15:30:41 +0900

    feat : delete goal sidebar에 추가

--- src/features/delete-goal/model/useDeleteGoal.ts
+++ src/features/delete-goal/model/useDeleteGoal.ts
@@ -3,28 +3,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useCustomMutation } from '@/shared/lib/utils/useCustomMutation';
 import { dashboardQueryKeys } from '@/entities/dashboard';
-import { deleteStudy } from '../api';
+import { deleteGoals } from '../api';
 import { studyQueryKeys } from '@/entities/study';
 import { goalQueryKeys } from '@/entities/goal';
 import { useRouter } from 'next/navigation';
 
-interface DeleteStudyMutationParams {
+interface DeleteGoalMutationParams {
+  goalId: string;
   studyId: string;
 }
-export const useDeleteStudyMutation = (studyId: string) => {
+export const useDeleteGoalMutation = (goalId: string, studyId: string) => {
   const router = useRouter();
 
-  return useCustomMutation<DeleteStudyMutationParams, any>({
-    mutationFn: ({ studyId }) => deleteStudy(studyId),
+  return useCustomMutation<DeleteGoalMutationParams, any>({
+    mutationFn: ({ goalId }) => deleteGoals(goalId),
     invalidateQueryKeys: [
       //대시보드 쿼리 무효화
-      [...dashboardQueryKeys.goal(studyId)],
+      [...dashboardQueryKeys.goal(goalId)],
       //스터디 리스트 쿼리 무효화
       [...studyQueryKeys.list()],
       //목표 리스트 쿼리 무효화
       [...goalQueryKeys.list(Number(studyId))],
     ],
-    successMessage: '스터디가 삭제되었습니다',
+    successMessage: '목표가 삭제되었습니다',
     mutationOptions: {
       onSuccess: () => {
         router.push('/');
