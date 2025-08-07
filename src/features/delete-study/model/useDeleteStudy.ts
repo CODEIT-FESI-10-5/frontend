355f4cf030c80577d4eb81e1f4f935b288f43b9b
sdc9787 <sfc9787@gmail.com>
2025-08-07 14:48:46 +0900

    fix : 스터디 삭제 성공시 root로 이동

--- src/features/delete-study/model/useDeleteStudy.ts
+++ src/features/delete-study/model/useDeleteStudy.ts
@@ -1,16 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useCustomMutation } from '@/shared/lib/utils/useCustomMutation';
-import { todolistQueryKeys } from '@/entities/todolist/model';
 import { dashboardQueryKeys } from '@/entities/dashboard';
 import { deleteStudy } from '../api';
 import { studyQueryKeys } from '@/entities/study';
 import { goalQueryKeys } from '@/entities/goal';
+import { useRouter } from 'next/navigation';
 
 interface DeleteStudyMutationParams {
   studyId: string;
 }
-export const useDeleteStudyMutation = (studyId: string) =>
-  useCustomMutation<DeleteStudyMutationParams, any>({
+export const useDeleteStudyMutation = (studyId: string) => {
+  const router = useRouter();
+
+  return useCustomMutation<DeleteStudyMutationParams, any>({
     mutationFn: ({ studyId }) => deleteStudy(studyId),
     invalidateQueryKeys: [
       //대시보드 쿼리 무효화
@@ -21,4 +23,10 @@ export const useDeleteStudyMutation = (studyId: string) =>
       [...goalQueryKeys.list(Number(studyId))],
     ],
     successMessage: '스터디가 삭제되었습니다',
+    mutationOptions: {
+      onSuccess: () => {
+        router.push('/');
+      },
+    },
   });
+};
