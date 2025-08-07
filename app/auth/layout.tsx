44a7fc2d6f167bcdcf07e96f90cb65fc666b406a
memymini <minhi0614@gmail.com>
2025-08-07 16:35:55 +0900

    chore:auth 페이지 로고 클릭시 온보딩으로 이동

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 export default function AuthLayout({
   children,
 }: {
@@ -8,12 +9,14 @@ export default function AuthLayout({
     <div className="bg-surface-2 md:bg-surface-1 flex h-full w-full max-w-screen items-start justify-center px-24 py-32 md:items-center md:p-0">
       <div className="md:bg-surface-2 rounded-12 flex w-327 flex-col items-start justify-center gap-28 overflow-y-auto md:h-auto md:w-642 md:px-100 md:py-68">
         <div className="relative h-24 w-94 md:h-36 md:w-153">
-          <Image
-            src="/images/logo.png"
-            alt="logo"
-            fill
-            className="object-contain"
-          />
+          <Link href="/">
+            <Image
+              src="/images/logo.png"
+              alt="logo"
+              fill
+              className="object-contain"
+            />
+          </Link>
         </div>
         <div className="h-full w-full">{children}</div>
       </div>
