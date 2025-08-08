0f62b498e1de63aaf123b61e3012ae3f148f492d
sdc9787 <sfc9787@gmail.com>
2025-08-08 11:32:56 +0900

    fix : 미들웨어 로그인 확인만 하도록 동작

--- middleware.ts
+++ middleware.ts
@@ -43,8 +43,6 @@ export async function middleware(req: NextRequest) {
   const needsLoginCheck =
     isDashboard || isNote || isAccount || isRoot || isTodo;
 
-  const needsRoute = isLoginToDashboard || isRoot;
-
   if (needsLoginCheck) {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/api/auth/check`,
@@ -58,83 +56,9 @@ export async function middleware(req: NextRequest) {
       },
     );
     const loginRes: AuthCheckResponse = await res.json();
-    console.log(loginRes);
-
     if (loginRes.httpStatusCode !== 200) {
       return NextResponse.redirect(new URL('/auth/login', req.url));
     }
   }
-  // 로그인 O
-  if (needsRoute) {
-    const timestamp = Date.now();
-    const studyRes = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/study?_t=${timestamp}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Cache-Control': 'no-cache, no-store, must-revalidate',
-          Pragma: 'no-cache',
-          Expires: '0',
-          cookie,
-        },
-        cache: 'no-store',
-      },
-    );
-
-    if (!studyRes.ok) {
-      // 스터디 안불러지는 경우 에러처리 필요
-      return NextResponse.redirect(new URL('/', req.url));
-    }
-
-    const studyListRes: StudyListResponseApi = await studyRes.json();
-    const studyList = studyListRes.data;
-
-    if (studyListRes.httpStatusCode === 200 && studyList.totalCount !== 0) {
-      const goalTimestamp = Date.now();
-      const goalRes = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/studies/${studyList.studyList[0].studyId}/goals?_t=${goalTimestamp}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Cache-Control': 'no-cache, no-store, must-revalidate',
-            Pragma: 'no-cache',
-            Expires: '0',
-            cookie,
-          },
-          cache: 'no-store',
-        },
-      );
-
-      if (!goalRes.ok) {
-        // 목표 안불러지는 경우 에러처리 필요
-        return NextResponse.redirect(new URL('/', req.url));
-      }
-      const goalListRes: GoalListResponseApi = await goalRes.json();
-      const goalList = goalListRes.data;
-      if (goalListRes.httpStatusCode === 200 && goalList.totalCount !== 0) {
-        // 2. 스터디O, 목표O
-        return NextResponse.redirect(
-          new URL(
-            `/dashboard/study/${studyList.studyList[0].studyId}/goal/${goalList.goals[0].id}`,
-            req.url,
-          ),
-        );
-      } else {
-        // 2. 스터디O, 목표X
-        return NextResponse.redirect(
-          new URL(
-            `/dashboard/study/${studyList.studyList[0].studyId}`,
-            req.url,
-          ),
-        );
-      }
-    } else {
-      // 3. 스터디X, 목표X
-      return NextResponse.redirect(new URL(`/dashboard/study`, req.url));
-    }
-  }
-
   return NextResponse.next();
 }
