b87d790899a453430b623a481cf9aa39d5a6b01c
memymini <minhi0614@gmail.com>
2025-08-10 09:01:33 +0900

    chore:모달 UI 변경

--- src/features/join-study/ui/JoinStudyModal.tsx
+++ src/features/join-study/ui/JoinStudyModal.tsx
@@ -24,19 +24,17 @@ export default function JoinStudyModal() {
     close();
   };
   return (
-    <div className="rounded-12 bg-surface-2 border-border-emphasis relative flex h-303 w-327 flex-col gap-30 border-1 px-24 py-38 md:h-338 md:w-518 md:p-38">
-      <CloseIcon
-        className="absolute top-30 right-30 h-30 w-30"
-        onClick={close}
-      />
-      <div className="flex flex-col gap-12">
-        <p className="m-headline-medium md:headline-large text-text-white flex w-full items-start">
-          초대 코드 입력
-        </p>
-        <p className="m-body-small md:body-medium text-text-secondary">
+    <div className="rounded-12 bg-surface-2 relative flex h-303 w-327 flex-col gap-30 px-24 py-38 md:h-338 md:w-518 md:p-38">
+      <div className="flex w-full flex-col gap-16">
+        <div className="flex w-full justify-between">
+          <p className="headline-large text-text-white">초대코드 입력</p>
+          <CloseIcon className="h-30 w-30 cursor-pointer" onClick={close} />
+        </div>
+        <p className="body-medium text-text-secondary">
           팀장에게 받은 코드를 입력해 주세요.
         </p>
       </div>
+
       <form
         onSubmit={handleSubmit(onValid)}
         className="flex h-165 w-full flex-col justify-between"
