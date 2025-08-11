97c9381d64309c41e1d6af3c937f563271834c24
AnJaeSeong <dktmzh6@gmail.com>
2025-08-11 13:16:01 +0900

    feat: todolist-detail 뒤로가기 버튼 추가

--- src/pages/todolist-detail/TodolistPage.tsx
+++ src/pages/todolist-detail/TodolistPage.tsx
@@ -1,5 +1,16 @@
+import RouterBackButton from '@/shared/ui/RouterBackButton';
 import TodolistPanel from '@/widgets/todolist-detail/ui/TodolistPanel';
+import IconBack3 from '@/assets/icon-back3.svg';
 
 export default function TodolistPage() {
-  return <TodolistPanel />;
+  return (
+    <div className="flex flex-col gap-18">
+      <div className="hidden xl:flex">
+        <RouterBackButton>
+          <IconBack3 />
+        </RouterBackButton>
+      </div>
+      <TodolistPanel />
+    </div>
+  );
 }
